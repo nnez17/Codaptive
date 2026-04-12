f974d41d9ec7524205dac4f7ed6a66122dab61f9
aryareal <aryadwiputra193@gmail.com>
2026-04-12 13:37:17 +0700

    fix: update api baseurl to production

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://codaptive.vercel.app",
+  baseURL: import.meta.env.VITE_API_URL || "https://codaptive.vercel.app/_/backend",
   withCredentials: true,
 });
 
